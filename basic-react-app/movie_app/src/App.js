87e082cea483ecffbded11b5b879f38b84d07483
SnowLeopardMoonBear <43803127+SnowLeopardMoonBear@users.noreply.github.com>
2020-05-11 15:45:07 +0900

    how react works by rendering on DOM elements
    also trivial project setup config

--- basic-react-app/movie_app/src/App.js
+++ basic-react-app/movie_app/src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. FUCK
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// all components should have a render function
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        my first div!
+      </div>
+    )
+  }
 }
 
+// 이게 있어야 JS 모듈 시스템에서 컴포넌트를 가져올 수 있음
 export default App;
--- basic-react-app/movie_app/src/index.js
+++ basic-react-app/movie_app/src/index.js
@@ -1,9 +1,15 @@
+// react component를 불러와 DOM에 연결해주는 역할
+// role of this file is root of a react project
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// reactDOM is used to render UI on a website
+// reactDOM이 id가 root인 element에 render한다
+// react는 UI 라이브러리일 뿐
 ReactDOM.render(
   <React.StrictMode>
     <App />
