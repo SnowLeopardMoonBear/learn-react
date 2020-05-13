ac9c084dcc7fa584cb6f20bfc816a4f6f32bfc48
SnowLeopardMoonBear <43803127+SnowLeopardMoonBear@users.noreply.github.com>
2020-05-13 18:05:26 +0900

    update dumb component without state and update

--- basic-react-app/movie_app/src/Movie.js
+++ basic-react-app/movie_app/src/Movie.js
@@ -1,33 +1,48 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import './Movie.css'; // import css for this component
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "./Movie.css"; // import css for this component
 
-class Movie extends Component{
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired
-    } // to check type and requirement of a prop
-    // but not working well...
+class Movie extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+  }; // to check type and requirement of a prop
+  // but not working well...
 
-    render(){
-        return(
-            <div>
-                <h2>{this.props.title}</h2>
-                <MoviePoster poster={this.props.poster} />
-            </div>
-        )
-    }
-
+  render() {
+    return (
+      <div>
+        <h2>{this.props.title}</h2>
+        <MoviePoster poster={this.props.poster} />
+      </div>
+    );
+  }
 }
-class MoviePoster extends Component{
-
-    static propTypes = {
-        poster: PropTypes.string.isRequired
-    }
-    render(){
-        return(
-            <img src={this.props.poster} width="200px" height="280px" alt="movie poster"/>
-        )
-    }
+class MoviePoster extends Component {
+  static propTypes = {
+    poster: PropTypes.string.isRequired,
+  };
+  render() {
+    return (
+      <img
+        src={this.props.poster}
+        width="200px"
+        height="280px"
+        alt="movie poster"
+      />
+    );
+  }
 }
+
+// Example of dumb component: For which doesn't need state, didMount(), updating something, but only props.
+// function MoviePoster(poster) {
+//   return (
+//     <img
+//       src={poster} // doesn't need this.props.state because it's outside an object
+//       width="200px"
+//       height="280px"
+//       alt="movie poster"
+//     />
+//   );
+// }
 export default Movie;
