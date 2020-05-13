f71a0fd1f05eb402d898698fdd5f07847ecb75a9
SnowLeopardMoonBear <43803127+SnowLeopardMoonBear@users.noreply.github.com>
2020-05-13 17:48:59 +0900

    add loading with custom render function
    beware: bracket error
    read error message and use codeblock tab of VScode

--- basic-react-app/movie_app/src/App.js
+++ basic-react-app/movie_app/src/App.js
@@ -8,31 +8,6 @@ import "./App.css";
 class App extends Component {
   // COMPONENT has state
   state = {
-    movies: [
-      {
-        title: "Parasite",
-        poster:
-          "https://image.chosun.com/sitedata/image/202002/11/2020021100227_0.jpg",
-      },
-      {
-        title: "The Host",
-        poster: "https://t1.daumcdn.net/cfile/12110210A8F1224840",
-      },
-      {
-        title: "Mother",
-        poster:
-          "https://img1.daumcdn.net/thumb/C155x225/?fname=http%3A%2F%2Ft1.daumcdn.net%2Fcfile%2F1804A4214A02B527D6",
-      },
-      {
-        title: "Snowpiercer",
-        poster: "https://t1.daumcdn.net/cfile/036DD04B51B5236718",
-      },
-      {
-        title: "Barking Dogs Never Bite",
-        poster:
-          "https://img1.daumcdn.net/thumb/C155x225/?fname=http%3A%2F%2Ft1.daumcdn.net%2Fcfile%2F156F1B10AB15040D30",
-      },
-    ],
     greeting: "Hello"
   };
 
@@ -42,7 +17,36 @@ class App extends Component {
   }
 
   componentDidMount() {
-    setTimeout(() => {
+    setTimeout(()=>{
+      this.setState({
+        movies: [
+          {
+            title: "Parasite",
+            poster:
+              "https://image.chosun.com/sitedata/image/202002/11/2020021100227_0.jpg",
+          },
+          {
+            title: "The Host",
+            poster: "https://t1.daumcdn.net/cfile/12110210A8F1224840",
+          },
+          {
+            title: "Mother",
+            poster:
+              "https://img1.daumcdn.net/thumb/C155x225/?fname=http%3A%2F%2Ft1.daumcdn.net%2Fcfile%2F1804A4214A02B527D6",
+          },
+          {
+            title: "Snowpiercer",
+            poster: "https://t1.daumcdn.net/cfile/036DD04B51B5236718",
+          },
+          {
+            title: "Barking Dogs Never Bite",
+            poster:
+              "https://img1.daumcdn.net/thumb/C155x225/?fname=http%3A%2F%2Ft1.daumcdn.net%2Fcfile%2F156F1B10AB15040D30",
+          },
+        ],
+      })
+    }, 2000);
+    setTimeout(()=>{
       // always use this.setState() method to modify states
       // With setState, we can implement scroll dynamic loading! AWESOME!
       this.setState({
@@ -50,8 +54,15 @@ class App extends Component {
           title: "Okja",
           poster: "https://i.pinimg.com/originals/97/a3/c5/97a3c5ad34f0af11f4fb2e37ed845a25.jpg"
         }]
-      });
-    }, 1000);
+      })
+    }, 5000);
+  }
+  // Coding convention: custom funtion made by myself starts with '_' to distinguish it from react function
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    })
+    return movies
   }
 
   render() {
@@ -59,13 +70,7 @@ class App extends Component {
     return (
       // using map function will iterate iterables
       <div className="App">
-        {this.state.greeting}
-        {/* index is the index of an array provided */}
-        {this.state.movies.map((movie, index) => {
-          return (
-            <Movie title={movie.title} poster={movie.poster} key={index} />
-          ); // recommended to provide a unique key
-        })}
+        {this.state.movies ? this._renderMovies() : "Loading"}
       </div>
       // Don't append semicolons at the end of jsx statements. They will be rendered as string.
     );
