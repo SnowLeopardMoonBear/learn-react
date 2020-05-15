dbf94c02ad86e806ebe183c05b736124bce3342d
SnowLeopardMoonBear <43803127+SnowLeopardMoonBear@users.noreply.github.com>
2020-05-16 02:52:09 +0900

    encrypt password before
    at DB schema with bcrypt
    1. make salt
    2. only on password change
    3. encrypt with salt and plain password, encrypt

--- node-react-tutorial/boiler-plate/models/User.js
+++ node-react-tutorial/boiler-plate/models/User.js
@@ -1,35 +1,52 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require('bcrypt'); // password encryption
+const saltRounds = 10;
 
 // Make schema of the model
 const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        maxlength: 50
-    },
-    email: {
-        type: String,
-        trim: true,
-        unique: 1
-    },
-    password: {
-        type: String,
-        maxlength: 50
-    },
-    role: {
-        type: Number,
-        default: 0
-    },
-    image: String,
-    token: {
-        type: String
-    },
-    tokenExp: {
-        type: Number
-    }
-})
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: 1,
+  },
+  password: {
+    type: String,
+    maxlength: 50,
+  },
+  role: {
+    type: Number,
+    default: 0,
+  },
+  image: String,
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
+});
 // unique:1 means the property should be unique in the model
 
+// encrypt password
+userSchema.pre("save", function (next) {
+  var user = this; // represents the schema
+  if(user.isModified('password')){
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err)
+      bcrypt.hash(user.password, salt, function (err, hash){
+        if(err) return next(err)
+        user.password = hash // replace plain pw with hashed one
+        next()
+      })
+    })
+  }
+});
+
 // Make Mongoose model with schema above
-const User = mongoose.model('User', userSchema); // 모델을 몽구스 모델화
+const User = mongoose.model("User", userSchema); // 모델을 몽구스 모델화
 
 module.exports = { User };
