c961b3df006069689bb43bbe913fb84c6cf2b384
SnowLeopardMoonBear <43803127+SnowLeopardMoonBear@users.noreply.github.com>
2020-05-15 14:53:22 +0900

    create mongoose user model

---
+++ node-react-tutorial/boiler-plate/models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+// Make schema of the model
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxlength: s50
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: 1
+    },
+    password: {
+        type: String,
+        maxlength: 50
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    image: String,
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    }
+})
+
+// Make the schema Mongoose model
+const User = mongoose.model('User', userSchema); // 모델을 몽구스 모델화
+
+module.exports = {User};
