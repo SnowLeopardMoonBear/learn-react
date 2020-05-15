85267a854fd3c7e7f053aed77c71508ce1e73e5d
SnowLeopardMoonBear <43803127+SnowLeopardMoonBear@users.noreply.github.com>
2020-05-15 14:46:53 +0900

    make MongoDB cluster and connect
    MY first MongoDB cluster!

--- node-react-tutorial/boiler-plate/index.js
+++ node-react-tutorial/boiler-plate/index.js
@@ -2,5 +2,10 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://mindflo:Sm88.cZpwMW$uwG@boilerplate-hreqx.mongodb.net/test?retryWrites=true&w=majority',{
+    useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false
+}).then(()=>console.log('MongoDB connected...')).catch(err => console.log(err));
+
 app.get('/', (req, res)=> res.send("<h1>Hello, world!</h1>"))
 app.listen(port, ()=>{console.log(`Backend server listening at port ${port}!`)})
