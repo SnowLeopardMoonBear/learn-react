738354a53cd79df5e20fe211efc4f7a6b8ee30f8
SnowLeopardMoonBear <43803127+SnowLeopardMoonBear@users.noreply.github.com>
2020-05-17 02:13:29 +0900

    start redux

---
+++ node-react-tutorial/boiler-plate/client/src/_reducer/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+//import user from './user_reducer';
+// import comment from "./comment_reducer"
+
+// combine multiple reducers at root reducer 
+const rootReducer = combineReducers({
+    //user
+})
+
+export default rootReducer
