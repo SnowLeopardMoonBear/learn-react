73ce1709af3419473ccc8f7c61734c132fa90bea
SnowLeopardMoonBear <43803127+SnowLeopardMoonBear@users.noreply.github.com>
2020-06-03 23:51:46 +0900

    design square page structure

--- mandalart/src/pages/square_page.js
+++ mandalart/src/pages/square_page.js
@@ -4,7 +4,16 @@ class SquarePage extends React.Component {
     name: "기본props",
   };
   render() {
-    return <h1>Hi, I'm square page!</h1>;
+    return (
+      <div class="square-id">
+        <h1>Hi, I'm square page!</h1>
+        <h2>square name</h2>
+        <li>
+          <ul>list item to do</ul>
+          <ul>multiple to-do items</ul>
+        </li>
+      </div>
+    );
   }
 }
 export default SquarePage;
