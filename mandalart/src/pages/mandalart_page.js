2ac904689601fd7027bd0ebd00d008a858b631fe
SnowLeopardMoonBear <43803127+SnowLeopardMoonBear@users.noreply.github.com>
2020-06-03 23:56:12 +0900

    design mandalart main page structure

--- mandalart/src/pages/mandalart_page.js
+++ mandalart/src/pages/mandalart_page.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react";
 class MandalartPage extends React.Component {
-    static defaultProps = {
-        name: '기본props'
-    }
-    render(){
-        return (
-            <h1>Hi, I'm mandalart main page!</h1>
-        );
-    }
+  static defaultProps = {
+    name: "기본props",
+  };
+  render() {
+    return (
+        <div class="nine-square-wrapper">
+            <div class="nine-squares-with-square-id-props"/>
+        </div>
+    );
+  }
 }
 export default MandalartPage;
