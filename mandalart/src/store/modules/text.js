fd5bbece723fc87ef210f3b1ee61cb4b84de4c38
SnowLeopardMoonBear <43803127+SnowLeopardMoonBear@users.noreply.github.com>
2020-06-05 23:59:43 +0900

    Update text.js

--- mandalart/src/store/modules/text.js
+++ mandalart/src/store/modules/text.js
@@ -5,7 +5,7 @@ export const changeText = (squares) => ({ type: CHANGE_TEXT, squares });
 export const testStore = (squares) => ({ type: TEST_STORE, squares});
 
 // 베열의 초기값을 생성
-const arr = new Array(81);
+const arr = new Array(81); // 81칸짜리 어레이에 네모칸별 텍스트를 저장, 사용자단에서 어레이 만들어 보냄
 const initialState = {
   squares: arr,
   test: 'Nah'
