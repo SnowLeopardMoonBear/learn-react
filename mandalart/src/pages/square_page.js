436582a90d0895c9eed096f62a955fdc3c1b3cbf
SnowLeopardMoonBear <43803127+SnowLeopardMoonBear@users.noreply.github.com>
2020-06-08 15:42:36 +0900

    Update: connect() function
    1. connect로 square 페이지에 state와 dispatch하는 함수를 바인딩했음.
    2. 그러나 값 업데이트가 제대로 작동 안 함. 이벤트 발생시키는 엘러먼트, 커넥트, 액션, 리듀서 중 어디에 문제 있는지 확인해야

--- mandalart/src/pages/square_page.js
+++ mandalart/src/pages/square_page.js
@@ -1,21 +1,24 @@
 // input 다루는 건 https://velopert.com/3634 참조했습니다. 감사합니다!
 import React from "react";
 import { Link } from "react-router-dom";
-import { Update } from "../actions/index.js";
-import { Connect } from "react-redux";
+import { update } from "../actions/index.js";
+import { connect } from "react-redux";
 
-class SquarePage extends React.Component {
+class SquareContent extends React.Component {
   state = {inputText:"default"}
-  send = () => {
-    alert(this.state.inputText);
-  };
+
   // input 안의 값이 바뀔 때마다 발동되는 함수로, state의 inputText에 저장해준다.
   handleChange = (e) => {
     this.setState({inputText: e.target.value}) // 이벤트를 유발한 element의 주소에 접근해 내부 값을 받아와 state를 업데이트.
   }
+  dispatchUpdate = () => {
+    this.props.send(this.props.match.params.index, this.state.inputText);
+  }
+
   render() {
     return (
       <div className="square-id">
+        <h1>스퀘어 수정하기</h1>
         <h1> {this.props.match.params.content} </h1>
         <input
           id="input"
@@ -24,12 +27,22 @@ class SquarePage extends React.Component {
           onChange={this.handleChange}
         ></input>
         <h1>{this.state.inputText}</h1>
-        <Link to="/">
-          <button onClick={this.send}>전송</button>
-        </Link>
+        <button onClick={this.dispatchUpdate}>전송</button>
+        <Link to="/"><span>홈으로</span></Link>
+        <p>{this.props.content}</p>
       </div>
     );
   }
 }
-
+let mapStateToProps = (state)=>{
+  return{
+    content: state.content
+  };
+}
+let mapDispatchToProps = (dispatch) => {
+  return {
+    send: (index, newText) => dispatch(update(index,newText))
+  };
+}
+const SquarePage = connect(mapStateToProps, mapDispatchToProps)(SquareContent);
 export default SquarePage;
