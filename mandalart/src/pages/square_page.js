df6a96602ca3be1b3ff31c29135ab0aeefdb612b
SnowLeopardMoonBear <43803127+SnowLeopardMoonBear@users.noreply.github.com>
2020-06-08 19:30:58 +0900

    Update: Style of square_page
    1. 스퀘어 업데이트하는 페이지를 좀 더 이쁘게 꾸며봤습니다. 아직 구성요소 배치나 색상, 폰트를 좀 더 신경써야 함.

--- mandalart/src/pages/square_page.js
+++ mandalart/src/pages/square_page.js
@@ -2,51 +2,88 @@
 import React from "react";
 import { update } from "../actions/index.js";
 import { connect } from "react-redux";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  text-align: center;
+`;
+const Wrapper = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  background-color: #fff7ff;
+`;
+const Square = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  background-color: #fffcff;
+  border: 2px;
+  display: block;
+  border-color: #ff69b4;
+  border-style: solid;
+  word-break: break-all;
+  overflow: hidden;
+  width: 80px;
+  height: 80px;
+  font-size: 1.1rem;
+  font-weight: 400;
+  text-align: center;
+  color: #222222;
+`;
+const Edit = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 20px;
+  width:190px;
+`;
 
 class SquareContent extends React.Component {
-  state = {inputText:"default"};
+  state = { inputText: "default" };
 
   // input 안의 값이 바뀔 때마다 발동되는 함수로, state의 inputText에 저장해준다.
   handleChange = (e) => {
-    this.setState({inputText: e.target.value}); // 이벤트를 유발한 element의 주소에 접근해 내부 값을 받아와 state를 업데이트.
-  }
+    this.setState({ inputText: e.target.value }); // 이벤트를 유발한 element의 주소에 접근해 내부 값을 받아와 state를 업데이트.
+  };
   // 수정 버튼 클릭시 발동하는 함수
   dispatchUpdate = () => {
-    this.props.send(parseInt(this.props.match.params.index), this.state.inputText);
-    this.props.history.push('/');
-  }
+    this.props.send(
+      parseInt(this.props.match.params.index),
+      this.state.inputText
+    );
+    this.props.history.push("/");
+  };
   enterKey = (e) => {
-    if (e.key==='Enter'){
+    if (e.key === "Enter") {
       this.dispatchUpdate();
       alert("값이 변경되었습니다");
     }
-  }
+  };
 
   render() {
     return (
-      <div onKeyUp={this.enterKey}>
-        <h1>스퀘어 수정하기</h1>
-        <h1> {this.props.match.params.content} </h1>
-        <input
-          id="input"
-          type="text"
-          placeholder={this.props.match.params.content}
-          onChange={this.handleChange}
-        ></input>
-        <button onClick={this.dispatchUpdate}>전송</button>
-      </div>
+      <Wrapper onKeyUp={this.enterKey}>
+        <Title>스퀘어 수정하기</Title>
+        <Square> {this.props.match.params.content} </Square>
+        <Edit>
+          <input
+            type="text"
+            placeholder={this.props.match.params.content}
+            onChange={this.handleChange}
+          ></input>
+          <button onClick={this.dispatchUpdate}>전송</button>
+        </Edit>
+      </Wrapper>
     );
   }
 }
-let mapStateToProps = (state)=>{
-  return{
-    content: state.content
+let mapStateToProps = (state) => {
+  return {
+    content: state.content,
   };
-}
+};
 let mapDispatchToProps = (dispatch) => {
   return {
-    send: (index, newText) => dispatch(update(index,newText))
+    send: (index, newText) => dispatch(update(index, newText)),
   };
-}
+};
 const SquarePage = connect(mapStateToProps, mapDispatchToProps)(SquareContent);
 export default SquarePage;
