9ed75382ac0e0021bf1d40a6317ce955f619b1af
SnowLeopardMoonBear <43803127+SnowLeopardMoonBear@users.noreply.github.com>
2020-06-08 17:43:54 +0900

    Update: Redirect to Home When Store Updates
    1. Link 컴포넌트를 지움
    2. this.props.history.push() 를 통해 스토어 업데이트 후 자동으로 홈으로 리디렉션

--- mandalart/src/pages/square_page.js
+++ mandalart/src/pages/square_page.js
@@ -1,24 +1,24 @@
 // input 다루는 건 https://velopert.com/3634 참조했습니다. 감사합니다!
 import React from "react";
-import { Link } from "react-router-dom";
 import { update } from "../actions/index.js";
 import { connect } from "react-redux";
 
 class SquareContent extends React.Component {
-  state = {inputText:"default"}
+  state = {inputText:"default"};
 
   // input 안의 값이 바뀔 때마다 발동되는 함수로, state의 inputText에 저장해준다.
   handleChange = (e) => {
-    this.setState({inputText: e.target.value}) // 이벤트를 유발한 element의 주소에 접근해 내부 값을 받아와 state를 업데이트.
+    this.setState({inputText: e.target.value}); // 이벤트를 유발한 element의 주소에 접근해 내부 값을 받아와 state를 업데이트.
   }
   // 수정 버튼 클릭시 발동하는 함수
   dispatchUpdate = () => {
     this.props.send(parseInt(this.props.match.params.index), this.state.inputText);
+    this.props.history.push('/');
   }
   enterKey = (e) => {
     if (e.key==='Enter'){
-      this.dispatchUpdate()
-      alert("값이 변경되었습니다")
+      this.dispatchUpdate();
+      alert("값이 변경되었습니다");
     }
   }
 
@@ -33,7 +33,7 @@ class SquareContent extends React.Component {
           placeholder={this.props.match.params.content}
           onChange={this.handleChange}
         ></input>
-        <Link to="/"><button onClick={this.dispatchUpdate}>전송</button></Link>
+        <button onClick={this.dispatchUpdate}>전송</button>
       </div>
     );
   }
