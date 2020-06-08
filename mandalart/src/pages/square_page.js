8e6b6f58ff421d66a4a4de82b827a8303f5bdd16
SnowLeopardMoonBear <43803127+SnowLeopardMoonBear@users.noreply.github.com>
2020-06-08 13:30:12 +0900

    Update: save input value in state when input value changes

--- mandalart/src/pages/square_page.js
+++ mandalart/src/pages/square_page.js
@@ -1,13 +1,18 @@
+// input 다루는 건 https://velopert.com/3634 참조했습니다. 감사합니다!
 import React from "react";
 import { Link } from "react-router-dom";
 import { Update } from "../actions/index.js";
 import { Connect } from "react-redux";
 
 class SquarePage extends React.Component {
+  state = {inputText:"default"}
   send = () => {
-    alert("전송합니다");
+    alert(this.state.inputText);
   };
-
+  // input 안의 값이 바뀔 때마다 발동되는 함수로, state의 inputText에 저장해준다.
+  handleChange = (e) => {
+    this.setState({inputText: e.target.value}) // 이벤트를 유발한 element의 주소에 접근해 내부 값을 받아와 state를 업데이트.
+  }
   render() {
     return (
       <div className="square-id">
@@ -16,7 +21,9 @@ class SquarePage extends React.Component {
           id="input"
           type="text"
           placeholder={this.props.match.params.content}
+          onChange={this.handleChange}
         ></input>
+        <h1>{this.state.inputText}</h1>
         <Link to="/">
           <button onClick={this.send}>전송</button>
         </Link>
