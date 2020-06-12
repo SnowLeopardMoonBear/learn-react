cd96807dd86f98782ed2e05ad2f7c33429a19489
SnowLeopardMoonBear <43803127+SnowLeopardMoonBear@users.noreply.github.com>
2020-06-13 00:27:02 +0900

    Update: Change Default Array
    1. 디폴트 어레이를 좀 더 보기 쉬운 값으로 바꾸고
    2. store/helpers/default_array.js에 넣어 불러와 쓸 수 있게 함

--- mandalart/client/src/actions/index.js
+++ mandalart/client/src/actions/index.js
@@ -1,5 +1,5 @@
 // 만다라트 텍스트 배열을 업데이트하는 액션을 생성합니다.
-
+import defaultArray from "../reducers/helpers/default_array.js"
 export const UPDATE = "UPDATE";
 export const RESET = "RESET";
 
@@ -13,14 +13,6 @@ export function update(index, newSquare) {
 export function reset() {
   return {
     type: RESET,
-    initialArray: 
-      [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
-      11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 
-      21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 
-      31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 
-      '나의 만다라트', 42, 43, 44, 45, 46, 47, 48, 49, 50, 
-      51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 
-      61, 62, 63, 64, 65, 66, 67, 68, 69, 70,
-      71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81]
+    initialArray: defaultArray
   };
 }
